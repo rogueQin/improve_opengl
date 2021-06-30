4d1527a5dfe4337e17ca4b1f7e0d149ab0055d74
rogue <rogue_qin@163.com>
2021-06-30 14:02:43 +0800

    init project evn!

--- improve_opengl/LearnOpenGL/LearnOpenGL/main.cpp
+++ improve_opengl/LearnOpenGL/LearnOpenGL/main.cpp
@@ -1,19 +1,21 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
-
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+void processInput(GLFWwindow * window);
+
 int main() 
 {
+	// 初始化GLFW窗口
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// 创建GLFW窗口
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -23,6 +25,7 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	// 初始化GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD!" << std::endl;
@@ -34,18 +37,32 @@ int main()
 	
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+		// 处理输入事件
+		processInput(window);
 
+		// 处理渲染指令
+
+
+
+		// 检查并调用事件
+		glfwPollEvents();
+		// 交换缓冲区
+		glfwSwapBuffers(window);
+	}
 
 	glfwTerminate();
 	return 0;
 }
-
 
 void framebuffer_size_callback(GLFWwindow * window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow * window) 
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
