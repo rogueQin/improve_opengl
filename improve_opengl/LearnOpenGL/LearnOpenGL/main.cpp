fca5bfa6714df34476bcc090ba59f10cdd4853af
rogue <rogue_qin@163.com>
2021-07-05 00:27:47 +0800

    01_hello_triangle:  抽空搞清楚  VAO   VBO   GLDrawElement的原理和用法

--- improve_opengl/LearnOpenGL/LearnOpenGL/main.cpp
+++ improve_opengl/LearnOpenGL/LearnOpenGL/main.cpp
@@ -3,6 +3,31 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
+GLfloat vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+GLint success;
+GLchar infoLog[512];
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void processInput(GLFWwindow * window);
@@ -34,21 +59,93 @@ int main()
 
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// 顶点着色器
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error::shader::vertes::compilation_failed\n" << infoLog << std::endl;
+	}
+
+	// 片段着色器
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error::shader::fragemnt::compilation_failed\n" << infoLog << std::endl;
+	}
+
+	// 着色器程序
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Error::shader::program::compilation_failed\n" << infoLog << std::endl;
+	}
+
+	// 清理连接到程序对象的着色器
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	// 声明一个顶点缓冲对象
+	GLuint VBO;
+	// 初始化顶点缓冲对象
+	glGenBuffers(1, &VBO);
+
+	// 声明一个顶点数组对象
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+
 	
+	glBindVertexArray(VAO);
+	// 设置顶点缓冲对象缓冲区类型
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// 向缓冲区中写入数据
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 连接顶点属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	
+	glBindVertexArray(0);
+
+
+
 	while (!glfwWindowShouldClose(window))
 	{
+		// 检查并调用事件
+		glfwPollEvents();
 		// 处理输入事件
 		processInput(window);
 
 		// 处理渲染指令
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
+		// 使用着色器程序对象
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0,3);
+		glBindVertexArray(0);
 
-
-		// 检查并调用事件
-		glfwPollEvents();
 		// 交换缓冲区
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 	return 0;
