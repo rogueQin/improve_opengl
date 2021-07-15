4a92e33fe7c45e85d9bd3740c41c9b6ae19d9439
rogue <rogue_qin@163.com>
2021-07-15 23:17:58 +0800

    input & output

--- improve_opengl/LearnOpenGL/LearnOpenGL/main.cpp
+++ improve_opengl/LearnOpenGL/LearnOpenGL/main.cpp
@@ -16,18 +16,23 @@ unsigned int indices[] = {
 	2,3,0
 };
 
-const GLchar* vertexShaderSource = "#version 330 core\n"
+const GLchar* vertexShaderSource = 
+"#version 330 core\n"
 "layout (location = 0) in vec3 position;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"vertexColor = vec4(0.5,0.0,0.0,1.0);\n"
 "}\0";
 
-const GLchar* fragmentShaderSource = "#version 330 core\n"
+const GLchar* fragmentShaderSource = 
+"#version 330 core\n"
 "out vec4 color;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"color = vertexColor;\n"
 "}\n\0";
 
 GLint success;
