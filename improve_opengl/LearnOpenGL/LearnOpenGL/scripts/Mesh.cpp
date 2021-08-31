dc99b0ac39fc2f55d1a1160d2df81c4ae17c53cc
rogueQin <rogue_qin@163.com>
2021-08-31 18:58:07 +0800

    22_model_mesh

---
+++ improve_opengl/LearnOpenGL/LearnOpenGL/scripts/Mesh.cpp
@@ -0,0 +1,68 @@
+#include "Mesh.h"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures) 
+{
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	setupMesh();
+}
+
+void Mesh::Draw(Shader shader)
+{
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+
+	for (unsigned int i =0; i < textures.size(); i ++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i);
+		std::string number;
+		std::string name = textures[i].type;
+
+		if (name == "texture_diffuse")
+		{
+			number = std::to_string(diffuseNr++);
+		}
+		else if (name == "texture_specular")
+		{
+			number = std::to_string(specularNr++);
+		}
+		shader.setInt(("material." + name + number).c_str(), i);
+		glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	glActiveTexture(GL_TEXTURE0);
+}
+
+void Mesh::setupMesh() 
+{
+	// 声明一个顶点缓冲对象
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+		// 设置顶点缓冲对象缓冲区类型
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+
+		// 顶点索引
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()* sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+		// 顶点
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertices), (GLvoid*)0);
+		// 法线
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertices), (GLvoid*)offsetof(Vertex, Normal));
+		// 贴图
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(vertices), (GLvoid*)offsetof(Vertex, TexCoords));
+	glBindVertexArray(0);
+}
